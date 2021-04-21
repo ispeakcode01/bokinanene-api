98d661083f1f2fe0d18fdca8e43488fc9bc5740c
Kateti Mareko <katetimareko@codenertapps.com>
2021-04-21 16:49:14 +1200

    enable accessing environment variables

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 import graphqlServer from './api'
+import dotenv from 'dotenv'
 
+dotenv.config()
 const server = express()
 
 server.use(graphqlServer.getMiddleware())
