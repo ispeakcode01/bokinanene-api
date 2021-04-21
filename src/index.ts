fe6b952c034ffa3dcc07621a65ea77d6a43a5bc5
Kateti Mareko <katetimareko@codenertapps.com>
2021-04-21 16:50:15 +1200

    Connect to database

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 import server from './server'
+import config from './api/db/config'
+import db from './api/db'
+
+if (process.env.NODE_ENV === 'development') {
+    db(config.development)
+}
 
 const PORT = process.env.PORT || 3000
 
